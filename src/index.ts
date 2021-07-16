0d0e80117b846c3b2fc5a8aa1953705c5db6ca2d
Sarzy <adigwegodswillchinonso@gmail.com>
2021-07-16 21:36:28 +0100

    abstract class implementation

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,26 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumberCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
-// const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 
 // const sorter = new Sorter(numbersCollection);
 // sorter.sort();
-// console.log(numbersCollection.data);
+numbersCollection.sort();
+console.log(numbersCollection.data);
 
-const characterCollection = new CharactersCollection('Xaayb');
-const sorter = new Sorter(characterCollection);
-sorter.sort();
-console.log(characterCollection.data);
+// const characterCollection = new CharactersCollection('Xaayb');
+// const sorter = new Sorter(characterCollection);
+// sorter.sort();
+// console.log(characterCollection.data);
+
+// const linkedlist = new LinkedList();
+// linkedlist.add(500);
+// linkedlist.add(-10);
+// linkedlist.add(-3);
+// linkedlist.add(4);
+
+// const sorter = new Sorter(linkedlist);
+// sorter.sort();
+// linkedlist.print();
